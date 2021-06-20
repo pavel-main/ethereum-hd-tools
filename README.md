2bdc33f0aa837dbe533d59cf37c8339da412a53c
Pavel Kiselyov <pavel@blockchain.com>
2021-06-20 17:26:59 +0100

    Update showcase

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ Set of utilities to manage BIP44-compatible Ethereum HD wallet.
 * [collector](cmd/collector) - collects funds from multiple derived addresses
 * [distributor](cmd/distributor) - distributes funds to multiple derived addresses
 
-# Building
+## Building
 
     $ git clone github.com/pavel-main/ethereum-hd-tools
     $ cd ethereum-hd-tools && make build
+
+## Showcase
+
+[![asciicast](https://asciinema.org/a/421424.svg)](https://asciinema.org/a/421424)
+
+Transaction proofs on [Etherscan](https://rinkeby.etherscan.io/address/0x8888460E435D2DDff7108c12c50cc363c6057b8B)
--- scripts/demo.sh
+++ scripts/demo.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Set parameters
-SLEEP=30
+SLEEP=15
 CHAIN=4
-RPC=https://rinkeby.infura.io
+RPC=https://rinkeby.infura.io/v3/70a57660fc67493b8a9acac8e9fe39f2
 
 EACH=0.05
 TOTAL=0.6
@@ -16,10 +16,12 @@ XPUB=xpub6C2LnDVzUb4BdyDfTgSMg9beCUbYj898Za3ZeoENfdUqsmVd3dWeUEeLAXnm4zQa9PSozST
 XPRV=xprv9y2zNhy6eDVtRV9CMeuMK1eueSm4KfRHCM7xrQpm7HwrzyAUW6CPvSKrKEzunJxoYaDieB2MUDtT5hwFYeKb19wyMtf9THAh5kAaug7rTwy
 
 # Distribute and wait
+echo "📦 Distributing funds to 10 different derived addresses..."
 ./distributor --chain=$CHAIN --rpc=$RPC --xpub=$XPUB --prv=$PRV --from=0 --until=9 --amount=$EACH --random
-echo "Waiting 30s for blocks to be mined..."
+echo "Waiting 15s for blocks to be mined..."
 sleep $SLEEP
 
 # Collect
+echo "💰 Collecting funds from 10 different derived addresses..."
 ./collector --chain=$CHAIN --rpc=$RPC --from=0 --until=20 --xprv=$XPRV --destination=$DEST --amount=$TOTAL
-echo "Done"
+echo "✅ Done!"
